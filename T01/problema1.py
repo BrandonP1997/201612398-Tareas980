5a934ca1a2db57e88efa9fe134622ad786d45ef5
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2020-06-15 07:37:21 -0600

    Segunda version de tareas

---
+++ T01/problema1.py
@@ -0,0 +1,50 @@
+
+def division(num):
+    if(num>1):
+        dividendos = list(range(1,num))
+        Sparcial = 0
+        for i in dividendos:
+            cociente = num%i
+            if (cociente==0):
+                Sparcial = Sparcial + i
+    else:
+        Sparcial = 0
+    return Sparcial     
+          
+def BuscarNumsP(num):
+    posibles = list(range(2,num + 1))
+    NPer = []
+    for i in posibles:
+        
+        divisores = list(range(1, i ))
+        sumaiesimo = 0
+        for j in divisores:
+            cociente = i % j
+            if (cociente==0):
+                sumaiesimo = sumaiesimo + j
+        if(sumaiesimo==i):
+            NPer.append(i)
+
+    return NPer            
+
+def BuscarNumsA(num):
+    posibles = list(range(2,num + 1))
+    NA = []
+    friends = []
+
+    for i in posibles:
+        divisores = list(range(1, i))
+        sumaiesimo = 0
+        for j in divisores:
+            cociente = i % j
+            if (cociente==0):
+                sumaiesimo = sumaiesimo + j
+        NA.append(sumaiesimo)
+
+    for i in NA:
+        for j in range(len(posibles)):
+            if (division(i)==posibles[j]):
+                friends.append((posibles[j], NA[j]))
+    return friends  
+
+
